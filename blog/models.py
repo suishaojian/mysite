0a0b1bf0d95d9a83eee5323ef8506a5b68700960
suishaojian <daniel@finstone>
2016-10-10 16:21:10 +0800

    first commit

---
+++ blog/models.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    timestamp = models.DateTimeField()
